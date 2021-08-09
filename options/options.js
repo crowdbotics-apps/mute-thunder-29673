8d4713c8fed77a5622042640a7920ea95f6b6322
Crowdbotics <team@crowdbotics.com>
2021-08-09 19:58:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mute_thunder_29673Identifier",
+  url: "https://mute_thunder_29673Identifier.botics.co",
+  api: "https://mute_thunder_29673Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
